3960471bda053ea168c87082b6fd68cbe83d647a
EchelonAI-tech <135601084+EchelonAI-tech@users.noreply.github.com>
2023-06-10 12:56:00 -0400

    Update app.py
    Signed-off-by: EchelonAI-tech <135601084+EchelonAI-tech@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 import streamlit as st
 import numpy as np
-from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
